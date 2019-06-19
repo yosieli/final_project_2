0bf061d5e87d7e5bfba454cd6b3524b306ff7bc1
feven tsegay <feventsegay6@gmail.com>
2019-06-19 16:26:09 -0500

    style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,25 +6,39 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-       # @book=@user.books
-        
     end
 
 
     def new 
-       @user=User.new 
-       
+    @user=User.new 
     end
 
     def display_login_form
+       
+        
+        if flash[:error]
+            @error=flash[:error]
+        
+        else
+            @error = {
+                "user_name" => []
 
+            }
+    
     end
 
     def create
         
-        @user=User.create(strong_params)
+        @user.assign_attributes(strong_params)
+        if(@user.valid?)
+            @user.save
+            redirect_to  @user
+        else
+            flash[:error] = @user.errors
+            redirect_to "/login"
+        end
         
-        redirect_to "/login"
+       
     end
 
     def edit
@@ -39,6 +53,7 @@ class UsersController < ApplicationController
     end
 
     def authenticate
+
         # The username the use wrote to find the user 
         user= User.find_by(user_name: params[:user_name])
     
@@ -49,8 +64,12 @@ class UsersController < ApplicationController
              # If it was, save the users id in the session
             session[:user_id] = user.id
             redirect_to "/users/#{user.id}"
-        else 
-            # oops
+          else 
+            
+            
+            
+          end
+             
         end
     end
 
