7757b3702cf9f8a86a53f45c0a24ded5a8fe7a05
Jason Fournier <jmf4nier@hotmail.com>
2019-06-20 14:24:53 -0500

    errors fixed

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,43 +30,25 @@ class BooksController < ApplicationController
       @@search_result = api_search(strong_params[:search])
       redirect_to "/books/search"
     end
+  end
 
-    def search
+  def api_search(input)
+    response = RestClient.get("https://www.googleapis.com/books/v1/volumes?q=#{input}&maxResults=3&printType=books")
+    api_response = JSON.parse(response)
+    api_response
+  end
 
-    end
-
-    def search_form
-      
-      if strong_params[:book_list] == 'author'
-        @search_result = api_search("author:#{strong_params[:search]}")
-       byebug
-      elsif strong_params[:book_list] == 'title'
-        @search_result = api_search(strong_params[:search])
-        byebug
-      end
-    end
-
-    def api_search
-      response = RestClient.get("https://www.googleapis.com/books/v1/volumes?q=dog&maxResults=3&printType=books")
-      api_response = JSON.parse(response)
-      api_response
+  def api_image_search
+    img_url = nil
+    @@search_result['items'].each do |book|
+      self_link = RestClient.get(book['selfLink'])
+      item_info = JSON.parse(self_link)
+      img_url = item_info['volumeInfo']['imageLinks']['small']  
     end
       img_url
   end
 
-    def search
-    
-    end
-
-    def destroy
-      @book = Book.find(params[:id])
-      @book.destroy
-    end
-  
-
-
-
-    def strong_params
+  def strong_params
      
     params.require(:book).permit([:title, :genre, :img_url, :author, :book_list, :search, :summary, :page_count])
 
@@ -74,5 +56,3 @@ class BooksController < ApplicationController
 
 
 end
-
-
